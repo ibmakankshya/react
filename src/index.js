82defdebc88c0df710712e3b1c92cb85126c3823
unknown <ibmjavanh06@iiht.tech>
2021-04-29 17:43:09 +0530

    some

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import  React from "react";
 import ReactDOM from "react-dom";
 
-import MyIBM from "./App";
+// import input from "./input";
 
 //ReactDOM.render(<Head />,document.getElementById('root'));
 
-ReactDOM.render(<MyIBM />,document.getElementById('root'));
+ //ReactDOM.render(<input />,document.getElementById('root'));
 //ReactDOM.render(<foot />,document.getElementById('foot'));
+class Bike extends React.Component{//inheritance
+    constructor(){
+        super();
+        this.state ={colour:"red"};//class based component
+    }
+    render(){
+    return(<p>This Bike  is {this.state.colour
+    } in colour</p>)
+    }
+}
+
+    ReactDOM.render(<Bike />,document.getElementById('root'));
---
+++ src/input.js
@@ -0,0 +1,10 @@
+
+function input(){
+   return(<div>
+       <input placeholder="Colour" id="inputs"></input>
+   </div>) 
+}
+if(document.getElementById('inputs')==='Red'){
+    console.log("hi");
+}
+export default input;
