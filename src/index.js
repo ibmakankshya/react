61b61e07175bcf48fee9a338c779009bf6d31e8d
unknown <ibmjavanh06@iiht.tech>
2021-04-30 16:20:21 +0530

    setstate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import  React from "react";
 import ReactDOM from "react-dom";
-import AppMy from "./csshow";
+//import AppMy from "./csshow";
 //import './styles.css'
 
 //ReactDOM.render(<Head />,document.getElementById('root'));
@@ -64,4 +64,44 @@ import AppMy from "./csshow";
     //       return <h3 style={mystyle}> How have you been?</h3>;
     //       }
     //   }
-    ReactDOM.render(<AppMy  />, document.getElementById('root'));
+
+    class App extends React.Component { 
+        constructor(props){ 
+          super(props) ;
+              
+          // Set initial state 
+          this.state = {fruit : 
+                "Apple"} ;
+              
+          // Binding this keyword 
+          //this.updateState = this.updateState.bind(this) 
+        } 
+          
+        // updateState(){ 
+        //   // Changing state 
+        //   this.setState({fruit : 
+        //                'Mango'}) 
+        // } 
+        //simplest static derived
+        // static getDerivedStateFromProps(props,state){
+        //     return {fruit:props.favcol};
+        // }
+            componentDidMount(){
+                setTimeout(()=>{
+                    this.setState=({fruit:"Grapes"})
+                },1000)
+            }
+        render(){ 
+          return (  <div> 
+            <h2>This is a:</h2>   
+            <p>{this.state.fruit}</p>
+           
+             {/* Set click handler */} 
+             {/* <button onClick={this.updateState}> 
+               Click me!
+             </button>  */}
+           </div> 
+         ) 
+       } 
+     } 
+    ReactDOM.render(<App  favcol="Litchi"/>, document.getElementById('root'));
